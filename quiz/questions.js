0e514054a8020041b9beabb215367b44a1af41e6
Dora Topi <Dora@doraki.onmicrosoft.com>
2019-11-07 10:17:37 +0100

    Commit

---
+++ quiz/questions.js
@@ -0,0 +1,14 @@
+var questions = [
+  ["Who is the director of Joker??", "Shea Whigham", "Joaquin Phoenix ", "Todd Phillips","None", "3"],
+  ["What's the Joker's real name?", "	Warner Bros", "Jack Napier", " Scott Silver", "Dont Know","2"],
+  ["Is Arthur Fleck Thomas Wayne's son??", "Yes", "No", "Dont Know", "Maybe","1"],
+  ["Who killed Bruce Wayne's parents?", " Batman", "Bradley Cooper", "Joe Chill", "Dont Know","3"],
+  ["Joker Is Set In Which Year?", " 1980", " 1981", " 1985", "Dont know","2"],
+  ["Who owns working title?", " Village Roadshow Pictures", "  DC Films", " Universal Studios", "Avg co","3"],
+  ["What Colour Is The Sign Arthur Gets Hit With?", "Red", "Yellow", "Black", "Blue","1"],
+  ["What Is Joker’s Uncontrollable Condition Called?", " Unscripted Laughing", "Uncontrollable Laugh", "Pathological Laughing", "Psychotic Laughing","2"],
+  ["What Is Murray’s Catchphrase?", "“That’s Life”", "See You Tomorrow, Folks  ", "You Get What You Deserve", "See You Tomorrow, Folks","1"],
+  ["What Is The Name Of Murray’s Chat Show?", "Late Night With Murray Franklinr", "Tonight With Murray Franklin", "Live With Murray Franklin", "Murray Live","3"],
+  ["What Is The Club Called That Arthur Performs His Stand Up At??", "Pogo’s", "Hop’s", "Bobo’s", "Boogie’s","2"],
+  ["Who Is Arthur’s ‘Love Interest’ Called?", "Susie", "Sophie", " Sally", "Sandra","2"],
+  ];
